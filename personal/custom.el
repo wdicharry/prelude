820151d1cc5efd3eb6d4dc60eebb69a0d3a7bc9c
Will Dicharry <wdicharry@gmail.com>
2017-11-19 08:06:11 -0700

    Configure neotree.

--- personal/custom.el
+++ personal/custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(neo-theme (quote nerd))
  '(package-selected-packages
    (quote
     (cmake-mode neotree zop-to-char zenburn-theme which-key volatile-highlights undo-tree smartrep smartparens smart-mode-line operate-on-number move-text magit projectile ov imenu-anywhere guru-mode grizzl god-mode gitignore-mode gitconfig-mode git-timemachine gist flycheck expand-region epl editorconfig easy-kill diminish diff-hl discover-my-major dash crux browse-kill-ring beacon anzu ace-window)))
@@ -16,6 +17,7 @@
 
 (load-theme 'tango-dark)
 (set-frame-font "Inconsolata")
+(set-face-attribute 'default nil :height 100)
 
 (require 'clang-format)
 (setq clang-format-style-option "llvm")
